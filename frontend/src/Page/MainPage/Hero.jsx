c5e7eb4e59ab32f41a0e2ad81770959f7a6b6f0f
qkdwn <bjhbjh0313@gmail.com>
2025-07-22 13:06:07 +0900

    Ch2-1. 메인 페이지의 메인 배너(Hero.jsx) 제작

--- frontend/src/Page/MainPage/Hero.jsx
+++ frontend/src/Page/MainPage/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Hero = () => {
   return (
     <div className="realative min-h-[110vh] bg-gradient-to-b from-gray-50 to-white pb-0">
-      <div className="container mx-0 px-4 py-28">
+      <div className="container mx-0 px-4 sm:px-6 lg:px-8 py-28 lg:py-44">
         <div className="flex flex-col lg:flex-row items-center justify-between gap-12">
           <h1 className="text-3xl font-bold text-gray-900 leading-tight mb-6">
             태양광 설비 전문가와 함께
@@ -15,6 +15,9 @@ const Hero = () => {
           <div className="flex flex-col gap-4 justify-center">
             <button className="px-8 py-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300 text-lg font-semibold shadow-lg hover:shadow-xl">
               상담 신청하기
+            </button>
+            <button className='px-8 py-4 bg-white text-blue-600 rounded-lg border-2 border-blue-600 hover:bg-blue-50 transition-color duration-300 text-lg font-semibold'>
+              더 알아보기
             </button>
           </div>
         </div>
