e3e8034b15bc51b77148ba2b387071b8837c4f85
qkdwn <bjhbjh0313@gmail.com>
2025-07-23 10:39:39 +0900

    Ch2-2. 메인 페이지의 게시판(Forum.jsx) 제작

--- frontend/src/Page/MainPage/Forum.jsx
+++ frontend/src/Page/MainPage/Forum.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Forum = () => {
   const dummyPosts = [
@@ -43,6 +44,23 @@ const Forum = () => {
       createdAt: "2023-01-20",
     },
   ]
+
+  return (
+    <div className="bg-white">
+      <div className="container mx-auto px-4 py-28 lg:py-0 max-w-6xl">
+        <div className="text-center mb-6">
+          <h2 className="text-4xl lg:text-5xl font-bold text-gray-900">업무 게시판</h2>
+        </div>
+
+        <div className="flex justify-end mb-4">
+          <Link
+            to="/board"
+            className="px-5 py-2 bg-white text-gray-900 rounded-lg hover:bg-gray-100 transition-colors duration-300 flex items-center gap-2 border border-gray-200"
+          ></Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Forum
