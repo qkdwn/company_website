b26d429d69e0a3f92396add56571d3e451feef9e
qkdwn <bjhbjh0313@gmail.com>
2025-08-05 13:46:02 +0900

    Ch3-1. 회사 정보(About.jsx) 페이지

--- frontend/src/Page/About/About.jsx
+++ frontend/src/Page/About/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import companyImage from "../../assets/Image2.jpg"
 
 const About = () => {
   return (
-    <div>
+    <div className='container mx-auto px-4 py-32 max-w-7xl'>
+      <div className='relative rounded-2xl overflow-hidden shadow-2xl mb-24'>
+        <img src={companyImage} className='w-full h-full object-cover' />
+        <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-slate-900'></div>
+      </div>
       
     </div>
   )
