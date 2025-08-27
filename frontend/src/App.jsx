b83498655cf34bd83e303c76c07f8a902b066507
qkdwn <bjhbjh0313@gmail.com>
2025-08-27 11:16:52 +0900

    Ch5-4. (프론트엔드) 관리자 페이지 접근 제어

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,6 +44,32 @@ function AuthRedirectRoute() {
   return isAuthenticated ? <Navigate to="/admin/posts" replace /> : <Outlet />
 }
 
+function ProtectedRoute() {
+  const [isAuthenticated, setIsAuthenticated] = useState(null)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await axios.post("http://localhost:3000/api/auth/verify-token", {}, { withCredentials: true })
+        setIsAuthenticated(response.data.isValid)
+        setUser(response.data.user)
+      } catch (error) {
+        console.log("토큰 인증 실패: ", error)
+        setIsAuthenticated(false)
+        setUser(null)
+      }
+    }
+    verifyToken()
+  }, [])
+
+  if (isAuthenticated === null) {
+    return null
+  }
+
+  return isAuthenticated ? <Outlet context={{ user }} /> : <Navigate to="/admin" replace />
+}
+
 function Layout() {
   return (
     <>
@@ -101,23 +127,28 @@ const router = createBrowserRouter([
   },
   {
     path: "/admin",
-    element: <AdminLayout />,
-    children: [
+    element: <ProtectedRoute />,
+    childern: [
       {
-        path: "posts",
-        element: <AdminPosts />,
-      },
-      {
-        path: "create-post",
-        element: <AdminCreatePost />,
-      },
-      {
-        path: "edit-post/:id",
-        element: <AdminEditPost />,
-      },
-      {
-        path: "contacts",
-        element: <AdminContacts />,
+        element: <AdminLayout />,
+        children: [
+          {
+            path: "posts",
+            element: <AdminPosts />,
+          },
+          {
+            path: "create-post",
+            element: <AdminCreatePost />,
+          },
+          {
+            path: "edit-post/:id",
+            element: <AdminEditPost />,
+          },
+          {
+            path: "contacts",
+            element: <AdminContacts />,
+          },
+        ],
       },
     ],
   },
